a0888ba4846293456b0f485bc0ba1af7324c807d
vinhphat_98 <dvphat207@gmail.com>
2020-12-15 02:32:33 +0700

    add unit test

---
+++ src/tests/index.js
@@ -0,0 +1,23 @@
+/**
+ * File: \src\tests\index.js
+ * Project: seminar-software-testing
+ * Created Date: Tuesday, December 15th 2020, 2:28:25 am
+ * Author: Vĩnh Phát
+ * -----
+ * Last Modified:
+ * Modified By:
+ * ------------------------------------
+ */
+
+const { describe, it } = require('mocha')
+
+describe('Seminar Software Testing', function () {
+  it('Success', function (done) {
+    const flag = 5
+    if (flag === 5) {
+      done()
+    } else {
+      done(new Error('Failed.'))
+    }
+  })
+})
